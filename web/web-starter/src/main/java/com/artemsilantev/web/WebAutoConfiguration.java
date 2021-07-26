e2eb557fdb43e6522aadb2563272b7f136ec515b
artemsilantev <artem.silantev.2001@gmail.com>
2021-07-26 13:12:44 +0300

    add swagger-ui

--- web/web-starter/src/main/java/com/artemsilantev/web/WebAutoConfiguration.java
+++ web/web-starter/src/main/java/com/artemsilantev/web/WebAutoConfiguration.java
@@ -2,8 +2,10 @@ package com.artemsilantev.web;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 @ComponentScan(basePackages = {"com.artemsilantev.web"})
 public class WebAutoConfiguration {
 }
